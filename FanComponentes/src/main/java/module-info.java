ebdb4af6584767bb14fefed5972a1ac46f275112
FerminOsi <fosinagalo@educacion.navarra.es>
2024-04-30 19:41:02 +0200

    Updated

--- FanComponentes/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.fancomponentes {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.fancomponentes to javafx.fxml;
-    exports com.example.fancomponentes;
-}
