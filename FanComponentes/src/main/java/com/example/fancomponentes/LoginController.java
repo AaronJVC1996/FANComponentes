c4317b9f5edb99d2fd883fc91fe0bce76871c47d
AaronJVC1996 <avasquecar1@educacion.navarra.es>
2024-04-30 20:35:42 +0200

    He creado LoginController bien definido y ahora queda crear la vista bien y hacer que pase a una siguiente pagina

---
+++ FanComponentes/src/main/java/com/example/fancomponentes/LoginController.java
@@ -0,0 +1,54 @@
+package com.example.fancomponentes;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    // Método para verificar las credenciales del usuario
+    @FXML
+    protected void onLoginButtonClicked() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Conectar a la base de datos
+        try (Connection conn = DatabaseConnector.conectar()) {
+            // Verificar las credenciales del usuario en la base de datos
+            if (verificarCredenciales(conn, username, password)) {
+                System.out.println("Inicio de sesión exitoso");
+                // Aquí puedes abrir una nueva ventana o realizar alguna acción después de iniciar sesión
+            } else {
+                System.out.println("Credenciales incorrectas");
+                // Aquí puedes mostrar un mensaje de error al usuario
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Manejar cualquier error de conexión o consulta SQL
+        }
+    }
+
+    // Método para verificar las credenciales del usuario en la base de datos
+    private boolean verificarCredenciales(Connection conn, String username, String password) throws SQLException {
+        String sql = "SELECT * FROM ROLES WHERE NOMBRE=? AND CONTRASEÑA=?";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            return resultSet.next(); // Si hay resultados, las credenciales son válidas
+        }
+    }
+}
