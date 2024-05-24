97d866328f4b522157540004301eeb5b482bd98e
AaronJVC1996 <avasquecar1@educacion.navarra.es>
2024-05-24 16:55:32 +0200

    GestionDispositivos para probar

--- src/main/java/com/example/fancomponentes/GestionDispositivosController.java
+++ src/main/java/com/example/fancomponentes/GestionDispositivosController.java
@@ -1,11 +1,16 @@
 package com.example.fancomponentes;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
@@ -18,189 +23,114 @@ import java.sql.SQLException;
 
 public class GestionDispositivosController {
 
+    @FXML
+    private TableView<Dispositivo> dispositivosTableView;
 
+    @FXML
+    private TableColumn<Dispositivo, Integer> idColumn;
 
-        @FXML
-        private TableView<Componente> componentesTableView;
+    @FXML
+    private TableColumn<Dispositivo, String> nombreColumn;
 
-        @FXML
-        private TableColumn<Componente, String> idComponenteColumn;
+    @FXML
+    private TableColumn<Dispositivo, Double> precioColumn;
 
-        @FXML
-        private TableColumn<Componente, String> nombreColumn;
+    @FXML
+    private TableColumn<Dispositivo, Integer> stockColumn;
 
-        @FXML
-        private TableColumn<Componente, Integer> stockColumn;
+    @FXML
+    private Text descripcionText;
 
-        @FXML
-        private TableColumn<Componente, Double> precioColumn;
+    @FXML
+    private void initialize() {
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("nDispositivo"));
+        nombreColumn.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        precioColumn.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        stockColumn.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
 
+        dispositivosTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> mostrarDescripcion());
 
-        @FXML
-        private Text descripcionText;
+        dispositivosTableView.setOnMouseClicked(this::mostrarDescripcion);
+        dispositivosTableView.refresh();
+        cargarDispositivos();
+    }
 
-        @FXML
-        private Label lblTotalPrecio;
-        @FXML
-        private void initialize() {
+    private void cargarDispositivos() {
+        try (Connection conexion = DatabaseConnector.getConexion()) {
+            String consulta = "SELECT N_DISPOSITIVO, NOMBRE, PRECIO, DESCRIPCION, CANTIDAD FROM DISPOSITIVOS";
+            try (PreparedStatement declaracion = conexion.prepareStatement(consulta)) {
+                ResultSet resultado = declaracion.executeQuery();
+                ObservableList<Dispositivo> dispositivos = FXCollections.observableArrayList();
+                while (resultado.next()) {
+                    int nDispositivo = resultado.getInt("N_DISPOSITIVO");
+                    String nombre = resultado.getString("NOMBRE");
+                    double precio = resultado.getDouble("PRECIO");
+                    String descripcion = resultado.getString("DESCRIPCION");
+                    int cantidad = resultado.getInt("CANTIDAD");
 
-
-
-            // Cargar los nombres de los componentes desde la base de datos
-
-
-            // Configurar el listener para el evento de selección en el ListView (obtenido de internet)
-            componentesTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-                // Mostrar los detalles del empleado seleccionado
-                idComponenteColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-
-                nombreColumn.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-                precioColumn.setCellValueFactory(new PropertyValueFactory<>("precio"));
-                stockColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
-
-                componentesTableView.setOnMouseClicked(this::mostrarDescripcion);
-
-
-            });
-
-            componentesTableView.refresh();
-            cargarComponentes();
-
-            aplicarEstiloFilas();
-        }
-
-        // Metodo para cargar los nombres de los componentes desde la base de datos y mostrarlos en el ListView
-        private void cargarComponentes() {
-            try (Connection conexion = DatabaseConnector.getConexion()) {
-                String consulta = "SELECT idcomponente, nombre, stock, precio, descripcion FROM componentes";
-                try (PreparedStatement declaracion = conexion.prepareStatement(consulta)) {
-                    ResultSet resultado = declaracion.executeQuery();
-                    while (resultado.next()) {
-                        String id = resultado.getString("idcomponente");
-                        String nombre = resultado.getString("nombre");
-                        int stock = resultado.getInt("stock");
-                        double precio = resultado.getDouble("precio");
-                        String descripcion = resultado.getString("descripcion");
-
-                        // Create a new Componente object
-                        Componente componente = new Componente(id, nombre, stock, precio, descripcion);
-
-                        // Add the Componente object to the ListView
-                        this.componentesTableView.getItems().add(componente); // Displaying the name in the ListView
-                    }
+                    Dispositivo dispositivo = new Dispositivo(nDispositivo, nombre, precio, descripcion, cantidad);
+                    dispositivos.add(dispositivo);
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
+                dispositivosTableView.setItems(dispositivos);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void refrescarLista() {
+        dispositivosTableView.getItems().clear();
+        cargarDispositivos();
+    }
 
 
-
-
-
-        // Actualizar componentes
-
-
-
-        @FXML
-        private void refrescarLista() {
-            // limpia y vuelve a cargar los componentes
-            componentesTableView.getItems().clear();
-            cargarComponentes();
-        }
-        @FXML
-        private void aniadirStock() {
-            Componente componenteSeleccionado = componentesTableView.getSelectionModel().getSelectedItem();
-            if (componenteSeleccionado != null) {
-                try {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("AniadirStock.fxml"));
-                    Parent root = loader.load();
-
-                    AniadirStockController controller = loader.getController();
-                    controller.setComponenteId(componenteSeleccionado.getId());
-
-                    Stage stage = new Stage();
-                    stage.setScene(new Scene(root));
-                    stage.show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Alert alert = new Alert(Alert.AlertType.WARNING);
-                alert.setTitle("Ningún Componente Seleccionado");
-                alert.setContentText("Por favor, selecciona un componente para añadir stock.");
-                alert.showAndWait();
-            }
-        }
-        @FXML
-        private void aniadirComponente(ActionEvent event) {
-
+    /*
+    @FXML
+    private void aniadirStock() {
+         Dispositivo dispositivoSeleccionado = dispositivosTableView.getSelectionModel().getSelectedItem();
+        if (dispositivoSeleccionado != null) {
             try {
-                // Cargar la vista para agregar nuevo componente desde su archivo FXML
-                Parent agregar = FXMLLoader.load(getClass().getResource("NuevosComponentes.fxml"));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("AniadirStock.fxml"));
+                Parent root = loader.load();
+
+                AniadirStockController controller = loader.getController();
+                controller.setDispositivoId(dispositivoSeleccionado.getNDispositivo());
+
                 Stage stage = new Stage();
-                stage.setScene(new Scene(agregar));
+                stage.setScene(new Scene(root));
                 stage.show();
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Ningún Dispositivo Seleccionado");
+            alert.setContentText("Por favor, selecciona un dispositivo para añadir stock.");
+            alert.showAndWait();
         }
-        @FXML
-        private void mostrarDescripcion(MouseEvent event) {
-            Componente selectedComponente = componentesTableView.getSelectionModel().getSelectedItem();
-            if (selectedComponente != null) {
-                descripcionText.setWrappingWidth(200);  // Ajusta el valor según necesites
-                descripcionText.setText(selectedComponente.getDescripcion());
-            }
+    }
+
+    @FXML
+    private void aniadirDispositivo(ActionEvent event) {
+        try {
+            Parent agregar = FXMLLoader.load(getClass().getResource("NuevosDispositivos.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(agregar));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        private void aplicarEstiloFilas() {
-            componentesTableView.setRowFactory(tv -> new TableRow<Componente>() {
-                @Override
-                protected void updateItem(Componente item, boolean empty) {
-                    super.updateItem(item, empty);
-                    if (item == null) {
-                        setStyle("");
-                    } else {
-                        Integer stock = item.getStock(); // Usamos Integer en lugar de int
-                        if (stock == null) {
-                            setStyle("");
-                        } else {
-                            if (stock <= 5 && stock > 0) {
-                                getStyleClass().removeAll("low-stock", "out-of-stock");
-                                getStyleClass().add("low-stock");
-                            } else if (stock == 0) {
-                                getStyleClass().removeAll("low-stock", "out-of-stock");
-                                getStyleClass().add("out-of-stock");
-                            } else {
-                                getStyleClass().removeAll("low-stock", "out-of-stock");
-                            }
-                        }
-                    }
-                }
-            });
+     */
+
+    @FXML
+    private void mostrarDescripcion() {
+        Dispositivo selectedDispositivo = dispositivosTableView.getSelectionModel().getSelectedItem();
+        if (selectedDispositivo != null) {
+            descripcionText.setWrappingWidth(200); // Ajusta el valor según necesites
+            descripcionText.setText(selectedDispositivo.getDescripcion());
         }
-
-
-    // Método para sumar stock de un componente seleccionado
-    @FXML
-    private void sumarStock() {
-        // Lógica para sumar stock
     }
-
-    // Método para restar stock de un componente seleccionado
-    @FXML
-    private void restarStock() {
-        // Lógica para restar stock
-    }
-
-    // Método para construir con los componentes seleccionados
-    @FXML
-    private void construir() {
-        // Lógica para construir con los componentes seleccionados
-    }
-
-    }
-
-
-
+}
